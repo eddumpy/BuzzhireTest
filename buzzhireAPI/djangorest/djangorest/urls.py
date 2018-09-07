1a068fdbdbe24ba0af2e30101ccca915fa491157
Ed Dumpleton <eddumpleton@gmail.com>
2018-09-07 09:15:06 +0100

    added commented out url

--- buzzhireAPI/djangorest/djangorest/urls.py
+++ buzzhireAPI/djangorest/djangorest/urls.py
@@ -7,7 +7,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('events', views.EventView)
-
+#router.register(event/:id, views.EventId
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
